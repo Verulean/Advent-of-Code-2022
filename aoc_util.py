e6718ce4c259e380b37871e1a81789e7cef79735
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-01 23:49:39 -0600

    Days 1-2

---
+++ aoc_util.py
@@ -0,0 +1,18 @@
+from timeit import timeit
+import os
+
+
+def aoc_input(file_name, input_dir="input", cast_type=str, strip=True, sep='\n', file_prefix=''):
+    cwd = os.path.dirname(__file__)
+    with open(os.path.join(cwd, input_dir, file_prefix + file_name), 'r') as f:
+        if sep is None:
+            return f.read()
+        return [cast_type(i.strip()) if strip else cast_type(i) for i in f.read().split(sep=sep)]
+
+def time_to_string(n, solve, data, pad=11):
+    units = ((1e0, 's'), (1e-3, 'ms'), (1e-6, 'μs'), (1e-9, 'ns'))
+    t = timeit(lambda: solve(data), number=n) / n
+    
+    for magnitude, unit in units:
+        if t > magnitude:
+            return f"{t/magnitude:.4f} {unit}".rjust(pad)
---
+++ main.py
@@ -0,0 +1,41 @@
+from datetime import datetime as dt
+import pytz
+from importlib import import_module
+import aoc_util
+from pprint import pprint
+
+
+def current_day():
+    EST = pytz.timezone("America/New_York")
+    return dt.now(EST).day
+
+
+def main(
+    n=min(current_day(), 25),
+    time=False,
+    n_trials=1000,
+    print_solution=True,
+    file_variant="",
+):
+    aoc = import_module(f"days.aoc{n:02}{file_variant}")
+    fmt = getattr(aoc, "fmt_dict", {})
+    data = aoc_util.aoc_input(f"{n:02}.txt", **fmt)
+
+    if time:
+        t = aoc_util.time_to_string(n_trials, aoc.solve, data)
+        if n_trials == 1:
+            result = t
+        else:
+            result = f"average of {t} over {n_trials} runs."
+
+        print(f"Day {str(n).rjust(2)}: {result}")
+
+    solution = aoc.solve(data)
+    if print_solution:
+        pprint(solution)
+
+    return solution
+
+
+if __name__ == "__main__":
+    x = main()
