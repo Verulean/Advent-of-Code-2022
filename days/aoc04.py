3b7320a62437fb2686468726f54a905978faa74a
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-04 10:17:00 -0600

    Update utils

--- days/aoc04.py
+++ days/aoc04.py
@@ -7,5 +7,5 @@ def solve(data):
     ).T
     return (
         np.count_nonzero(((l1 <= l2) & (h2 <= h1)) | ((l2 <= l1) & (h1 <= h2))),
-        np.count_nonzero(((l1 <= h2) & (h2 <= h1)) | ((l2 <= h1) & (h1 <= h2))),
+        np.count_nonzero(~((h1 < l2) | (h2 < l1))),
     )
