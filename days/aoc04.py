ce4b0b52a5250d8ca21a2e5ed9922130c15e6471
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-03 23:19:38 -0600

    Update aoc04.py

--- days/aoc04.py
+++ days/aoc04.py
@@ -1,12 +1,11 @@
-def f(line, part=2):
-    e1, e2 = line.split(",")
-    l1, h1 = map(int, e1.split("-"))
-    l2, h2 = map(int, e2.split("-"))
-    
-    if part == 1:
-        return (l1 <= l2 and h2 <= h1) or (l2 <= l1 and h1 <= h2)
-    return (l2 <= h1 <= h2) or (l1 <= h2 <= h1)
-    
-    
+import numpy as np
+
+
 def solve(data):
-    return sum(f(line, 1) for line in data), sum(map(f, data))
+    l1, h1, l2, h2 = np.array(
+        [list(map(int, line.replace(",", "-").split("-"))) for line in data]
+    ).T
+    return (
+        np.count_nonzero(((l1 <= l2) & (h2 <= h1)) | ((l2 <= l1) & (h1 <= h2))),
+        np.count_nonzero(((l1 <= h2) & (h2 <= h1)) | ((l2 <= h1) & (h1 <= h2))),
+    )
