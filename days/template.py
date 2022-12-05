c7748746a8f5fc1d61020edb4917d7ade7bf2bf5
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-04 20:53:58 -0600

    Update solution template
    More explicit imports

--- days/template.py
+++ days/template.py
@@ -1,8 +1,29 @@
-from collections import *
-from functools import *
+from collections import namedtuple, deque, Counter, defaultdict
+from functools import cache, cached_property, partial, reduce
 from itertools import *
 from operator import *
-from util import *
+from util import (
+    LETTERS,
+    VOWELS,
+    CONSONANTS,
+    Neighbors,
+    lmap,
+    smap,
+    minmap,
+    maxmap,
+    ints,
+    floats,
+    words,
+    ordch,
+    chunks,
+    parts,
+    is_unique,
+    factors,
+    gcd,
+    lcm,
+    md5,
+    sha256,
+)
 import numpy as np
 
 
