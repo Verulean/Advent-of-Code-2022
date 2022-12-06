a946b020f15c47bab51a6a76dcaa15bcc762daa7
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-05 23:35:01 -0600

    Update aoc06.py

--- days/aoc06.py
+++ days/aoc06.py
@@ -2,7 +2,7 @@ fmt_dict = {"sep": None}
 
 
 def message_start_index(datastream, marker_length):
-    for i in range(0, len(datastream) - marker_length + 1):
+    for i in range(len(datastream) - marker_length + 1):
         if len(set(datastream[i : i + marker_length])) == marker_length:
             return i + marker_length
 
