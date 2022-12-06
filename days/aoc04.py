12642db18b048a16e6915df6606f146c5c05cafe
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-05 20:31:49 -0600

    Update aoc04.py

--- days/aoc04.py
+++ days/aoc04.py
@@ -1,10 +1,9 @@
+from util import ints
 import numpy as np
 
 
 def solve(data):
-    l1, h1, l2, h2 = np.array(
-        [list(map(int, line.replace(",", "-").split("-"))) for line in data]
-    ).T
+    l1, h1, l2, h2 = np.array([ints(line, False) for line in data]).T
     return (
         np.count_nonzero(((l1 <= l2) & (h2 <= h1)) | ((l2 <= l1) & (h1 <= h2))),
         np.count_nonzero(~((h1 < l2) | (h2 < l1))),
