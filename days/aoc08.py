8dd083fdb8fa3e776a102908251cd1127deae88a
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-08 01:28:30 -0600

    Rename variables

--- days/aoc08.py
+++ days/aoc08.py
@@ -2,15 +2,15 @@ from util import lmap
 import numpy as np
 
 
-def scan(vec, visible, viewing_distance):
+def scan(vec, visible, scenic_scores):
     indices = [-1] * 10
     for i, elem in enumerate(vec):
         j = max(indices[elem:])
         if j < 0:
             visible[i] = 1
-            viewing_distance[i] *= i
+            scenic_scores[i] *= i
         else:
-            viewing_distance[i] *= i - j
+            scenic_scores[i] *= i - j
         indices[elem] = i
 
 
@@ -18,13 +18,13 @@ def solve(data):
     grid = np.array([lmap(int, row) for row in data], dtype=int)
     m, n = grid.shape
 
-    V = np.zeros_like(grid)
-    S = np.ones_like(grid)
+    visible = np.zeros_like(grid)
+    scores = np.ones_like(grid)
 
     for i in range(m):
-        scan(grid[i], V[i], S[i])
-        scan(grid[i, ::-1], V[i, ::-1], S[i, ::-1])
+        scan(grid[i], visible[i], scores[i])
+        scan(grid[i, ::-1], visible[i, ::-1], scores[i, ::-1])
     for j in range(n):
-        scan(grid[:, j], V[:, j], S[:, j])
-        scan(grid[::-1, j], V[::-1, j], S[::-1, j])
-    return V.sum(), S.max()
+        scan(grid[:, j], visible[:, j], scores[:, j])
+        scan(grid[::-1, j], visible[::-1, j], scores[::-1, j])
+    return visible.sum(), scores.max()
