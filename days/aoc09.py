96b172a15da54705dd60f10a6341c3a1816f4fd6
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-09 00:17:20 -0600

    Update aoc09.py

--- days/aoc09.py
+++ days/aoc09.py
@@ -1,41 +1,40 @@
 import numpy as np
 
+
 DIRS = {
-    "U": np.array([-1, 0]),
-    "D": np.array([1, 0]),
-    "L": np.array([0, -1]),
-    "R": np.array([0, 1]),
+    "U": np.array([0, 1]),
+    "D": np.array([0, -1]),
+    "L": np.array([-1, 0]),
+    "R": np.array([1, 0]),
 }
 
 
-def parse_direction(line):
-    direction, length = line.split()
-    length = int(length)
-    return DIRS[direction], length
+def sign(x):
+    return 0 if x == 0 else -1 if x < 0 else 1
 
 
-def move_tail(h, t):
-    di, dj = h - t
-    if di == 0 and abs(dj) >= 2:
-        t[1] += np.sign(dj)
-    elif dj == 0 and abs(di) >= 2:
-        t[0] += np.sign(di)
-    elif not (abs(di) <= 1 and abs(dj) <= 1):
-        t[0] += np.sign(di)
-        t[1] += np.sign(dj)
+def trail(head, tail):
+    di, dj = head - tail
+    if di == dj == 0:
+        return False
+    if abs(di) > 1 or abs(dj) > 1:
+        tail[0] += sign(di)
+        tail[1] += sign(dj)
+    return True
 
 
 def solve(data):
-    LENGTH = 10
-    visited = set()
-    points = [np.array([0, 0]) for _ in range(LENGTH)]
-
+    knots = [np.zeros(2, dtype=int) for _ in range(10)]
+    v1 = {(0, 0)}
+    v2 = {(0, 0)}
     for line in data:
-        d, l = parse_direction(line)
-        for _ in range(l):
-            visited.add(tuple(points[-1]))
-            points[0] += d
-            for i in range(LENGTH - 1):
-                move_tail(points[i], points[i + 1])
-    visited.add(tuple(points[-1]))
-    return len(visited)
+        d, l = line.split()
+        offset = DIRS[d]
+        for _ in range(int(l)):
+            knots[0] += offset
+            for i in range(9):
+                if not trail(knots[i], knots[i + 1]):
+                    break
+            v1.add(tuple(knots[1]))
+            v2.add(tuple(knots[-1]))
+    return len(v1), len(v2)
