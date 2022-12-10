8d31ef45cde473cdc51667d1ca2d6f397cb57192
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-10 00:09:21 -0600

    Update aoc10.py

--- days/aoc10.py
+++ days/aoc10.py
@@ -1,42 +1,23 @@
-import numpy as np
+from util import chunks
 
 
-class CRT:
-    def __init__(self):
-        self.__x = 1
-        self.__cycle = 1
-        self.__signal_sum = 0
-        self.__screen = np.full((6, 40), ".")
-    
-    def parse(self, instr):
-        match instr.split():
-            case ["addx", n]:
-                self.__increment()
-                self.__increment(int(n))
-            case ["noop"]:
-                self.__increment()
-            case _:
-                raise ValueError(f"Invalid instruction: '{instr}'.")
-    
-    def __increment(self, value=0):
-        i, j = divmod(self.__cycle - 1, 40)
-        if abs(j - self.__x) <= 1:
-            self.__screen[i, j] = "#"
-        self.__cycle += 1
-        self.__x += value
-        if (self.__cycle - 20) % 40 == 0:
-            self.__signal_sum += self.__cycle * self.__x
-    
-    @property
-    def signal_strength(self):
-        return self.__signal_sum
-    
-    @property
-    def screen(self):
-        return "\n".join("".join(row) for row in self.__screen)
-                
 def solve(data):
-    Screen = CRT()
+    cycle = 1
+    x = 1
+    ans1 = 0
+    screen = ""
     for line in data:
-        Screen.parse(line)
-    return Screen.signal_strength, Screen.screen
+        _, *n = line.split()
+        i, j = divmod(cycle - 1, 40)
+        screen += (".", "#")[abs(j - x) <= 1]
+        cycle += 1
+        if cycle % 40 == 20:
+            ans1 += cycle * x
+        if n:
+            i, j = divmod(cycle - 1, 40)
+            screen += (".", "#")[abs(j - x) <= 1]
+            cycle += 1
+            x += int(n[0])
+            if cycle % 40 == 20:
+                ans1 += cycle * x
+    return ans1, "\n".join(chunks(screen, 40))
