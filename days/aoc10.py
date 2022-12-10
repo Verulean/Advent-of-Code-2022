c729384470f3a7bb90ce7d81445b3ecd8af9d886
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-10 00:21:48 -0600

    Update aoc10.py

--- days/aoc10.py
+++ days/aoc10.py
@@ -1,29 +1,19 @@
-from dataclasses import dataclass
+def step(state, value=0):
+    i, j = divmod(state[0] - 1, 40)
+    if j == 0 and i > 0:
+        state[3] += "\n"
+    state[3] += (".", "#")[abs(j - state[1]) <= 1]
+    state[0] += 1
+    state[1] += value
+    if state[0] % 40 == 20:
+        state[2] += state[0] * state[1]
 
 
-@dataclass
-class State:
-    cycle: int
-    x: int
-    signal_sum: int
-    screen: str
-
-    def step(self, value=0):
-        i, j = divmod(self.cycle - 1, 40)
-        if j == 0 and i > 0:
-            self.screen += "\n"
-        self.screen += (".", "#")[abs(j - self.x) <= 1]
-        self.cycle += 1
-        self.x += value
-        if self.cycle % 40 == 20:
-            self.signal_sum += self.cycle * self.x
-
-
-def solve(data):
-    state = State(1, 1, 0, "")
-    for line in data:
-        _, *n = line.split()
-        state.step()
+def solve(instructions):
+    state = [1, 1, 0, ""]
+    for instr in instructions:
+        _, *n = instr.split()
+        step(state)
         if n:
-            state.step(int(n[0]))
-    return state.signal_sum, state.screen
+            step(state, int(n[0]))
+    return state[2], state[3]
