0a2c2e91d24633866ed60c679cda1f51b65062a4
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-10 00:29:25 -0600

    Update aoc10.py

--- days/aoc10.py
+++ days/aoc10.py
@@ -1,9 +1,8 @@
 def step(state, value=0):
     cycle, x, sigsum, screen = state
     i, j = divmod(cycle - 1, 40)
-    if j == 0 and i > 0:
-        screen += "\n"
-    screen += (".", "#")[abs(j - x) <= 1]
+    if abs(j - x) <= 1:
+        screen[i][j] = "#"
     cycle += 1
     x += value
     if j == 18:
@@ -12,10 +11,10 @@ def step(state, value=0):
 
 
 def solve(instructions):
-    state = [1, 1, 0, ""]
+    state = [1, 1, 0, [["."] * 40 for _ in range(6)]]
     for instr in instructions:
         _, *n = instr.split()
         state = step(state)
         if n:
             state = step(state, int(n[0]))
-    return state[2], state[3]
+    return state[2], "\n".join(map("".join, state[3]))
