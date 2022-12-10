5cc5acb8b572f4c4a476ad08cebedffd213aad37
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-09 23:58:29 -0600

    Update aoc10.py

--- days/aoc10.py
+++ days/aoc10.py
@@ -1,38 +1,42 @@
-from util import chunks
+import numpy as np
 
 
+class CRT:
+    def __init__(self):
+        self.__x = 1
+        self.__cycle = 1
+        self.__signal_sum = 0
+        self.__screen = np.full((6, 40), ".")
+    
+    def parse(self, instr):
+        match instr.split():
+            case ["addx", n]:
+                self.__increment()
+                self.__increment(int(n))
+            case ["noop"]:
+                self.__increment()
+            case _:
+                raise ValueError(f"Invalid instruction: '{instr}'.")
+    
+    def __increment(self, value=0):
+        i, j = divmod(self.__cycle - 1, 40)
+        if abs(j - self.__x) <= 1:
+            self.__screen[i, j] = "#"
+        self.__cycle += 1
+        self.__x += value
+        if (self.__cycle - 20) % 40 == 0:
+            self.__signal_sum += self.__cycle * self.__x
+    
+    @property
+    def signal_strength(self):
+        return self.__signal_sum
+    
+    @property
+    def screen(self):
+        return "\n".join("".join(row) for row in self.__screen)
+                
 def solve(data):
-    X = 1
-    cycle = 1
-    i = 0
-    S = 0
-    SS = ""
-
-    def f(c, x):
-        return abs((c - 1) % 40 - x)
-
-    while i < len(data):
-        if i < len(data):
-            line = data[i]
-            cmd, *n = line.split()
-            if cmd == "noop":
-                SS += [".", "#"][f(cycle, X) <= 1]
-                cycle += 1
-                if (cycle - 20) % 40 == 0:
-                    S += cycle * X
-
-            elif cmd == "addx":
-                SS += [".", "#"][f(cycle, X) <= 1]
-                cycle += 1
-                if (cycle - 20) % 40 == 0:
-                    S += cycle * X
-
-                SS += [".", "#"][f(cycle, X) <= 1]
-                cycle += 1
-                X += int(n[0])
-                if (cycle - 20) % 40 == 0:
-                    S += cycle * X
-        i += 1
-
-    SS = "".join(line + "\n" for line in chunks(SS, 40))
-    return S, SS
+    Screen = CRT()
+    for line in data:
+        Screen.parse(line)
+    return Screen.signal_strength, Screen.screen
