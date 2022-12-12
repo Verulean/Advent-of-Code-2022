d34a88e9d08684502a22aeeb4be30d9118347d93
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-11 23:59:03 -0600

    Update aoc12.py

--- days/aoc12.py
+++ days/aoc12.py
@@ -1,48 +1,5 @@
-from util import lmap, ordch
+from util import dijkstra, lmap, ordch
 import numpy as np
-import heapq
-
-
-def neighbors(i, j):
-    return ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1))
-
-
-class PriorityQueue(list):
-    def pop(self):
-        return heapq.heappop(self)
-
-    def push(self, value):
-        return heapq.heappush(self, value)
-
-
-def dijkstra(
-    grid,
-    start,
-    end_condition,
-    adj_filter=lambda grid, node, adj: False,
-    cost_func=lambda grid, node, adj, cost: cost + 1,
-):
-    if getattr(end_condition, "__call__", None) is None:
-        END = end_condition
-        end_condition = lambda grid, node: node == END
-    m, n = grid.shape
-    q = PriorityQueue()
-    q.push((0, start))
-    g = np.full_like(grid, np.iinfo(grid.dtype).max)
-    g[start] = 0
-    while q:
-        cost, node = q.pop()
-        if end_condition(grid, node):
-            return g[node]
-        for adj in neighbors(*node):
-            if not (0 <= adj[0] < m and 0 <= adj[1] < n) or adj_filter(
-                grid, node, adj
-            ):
-                continue
-            new_cost = cost_func(grid, node, adj, cost)
-            if new_cost < g[adj]:
-                g[adj] = new_cost
-                q.push((new_cost, adj))
 
 
 def solve(data):
