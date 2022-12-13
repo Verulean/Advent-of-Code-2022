09e1cff5a6d93a6b780f5479a0bcbbe5a60f61b7
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-13 00:07:20 -0600

    Update aoc13.py

--- days/aoc13.py
+++ days/aoc13.py
@@ -1,49 +1,48 @@
 from util import chunks
 
 
-def f(a, b):
-    if isinstance(a, list) and isinstance(b, list):
-        la = len(a)  # 1
-        lb = len(b)  # 1
-        n = min(la, lb)
-        for i in range(n):
-            x = f(a[i], b[i])
-            if isinstance(x, bool):
-                return x
-        if la < lb:
-            return True
-        elif lb < la:
-            return False
-        return None
-    elif isinstance(a, int) and isinstance(b, int):
-        if a < b:
-            return True
-        elif a > b:
-            return False
-        return None
-    elif isinstance(b, list):
-        return f([a], b)
-    return f(a, [b])
+def compare(a, b):
+    la, lb = isinstance(a, list), isinstance(b, list)
+    if la and lb:
+        for aa, bb in zip(a, b):
+            if (r := compare(aa, bb)) is not None:
+                return r
+        return compare(len(a), len(b))
+    elif la:
+        return compare(a, [b])
+    elif lb:
+        return compare([a], b)
+    else:
+        return True if a < b else False if a > b else None
+
+
+def merge(A, left, right, end, B, f):
+    i, j = left, right
+    for k in range(left, end):
+        if i < right and (j >= end or compare(A[i], A[j])):
+            B[k] = A[i]
+            i += 1
+        else:
+            B[k] = A[j]
+            j += 1
+
+
+def merge_sort(A, f):
+    n = len(A)
+    B = [None] * n
+    l = 1
+    while l < n:
+        for i in range(0, n, l * 2):
+            merge(A, i, min(i + l, n), min(i + 2 * l, n), B, f)
+        A[:] = B
+        l *= 2
+    return A
 
 
 def solve(data):
-    ans1 = 0
-    i = 1
-    data = [eval(line) for line in data if line.strip()]
-
-    for a, b in chunks(data, 2):
-        if f(a, b):
-            ans1 += i
-        i += 1
-
+    data = [eval(line) for line in data if line]
+    ans1 = sum(i + 1 for i, args in enumerate(chunks(data, 2)) if compare(*args))
     data.extend([[[2]], [[6]]])
-    sorted_data = []
-    while data:
-        for i, elem in enumerate(data):
-            if all(f(elem, other) for other in data if other != elem):
-                del data[i]
-                sorted_data.append(elem)
-                break
-    ans2 = (sorted_data.index([[2]]) + 1) * (sorted_data.index([[6]]) + 1)
-
+    data = merge_sort(data, compare)
+    ans2 = (data.index([[2]]) + 1) * (data.index([[6]]) + 1)
     return ans1, ans2
