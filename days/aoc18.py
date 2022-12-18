c69473d375696bdfc9da0edd7bd0124c671697ec
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-17 23:50:15 -0600

    Update aoc18.py

--- days/aoc18.py
+++ days/aoc18.py
@@ -1,5 +1,5 @@
+from collections import deque
 from util import ints
-import numpy as np
 
 
 fmt_dict = {"cast_type": ints}
@@ -7,67 +7,48 @@ fmt_dict = {"cast_type": ints}
 
 def solve(data):
     cubes = set(map(tuple, data))
-
-    # part 1
-    S = 0
-    cuboid = np.zeros((20, 20, 20), dtype=int)
+    ans1 = 0
+    xl, yl, zl, xh, yh, zh = (0,) * 6
     for a, b, c in cubes:
-        cuboid[a, b, c] = 1
-        for other in [
+        xl = min(xl, a)
+        xh = max(xh, a)
+        yl = min(yl, b)
+        yh = max(yh, b)
+        zl = min(zl, c)
+        zh = max(zh, c)
+        for other in (
             (a + 1, b, c),
             (a - 1, b, c),
             (a, b + 1, c),
             (a, b - 1, c),
             (a, b, c + 1),
             (a, b, c - 1),
-        ]:
+        ):
             if other not in cubes:
-                S += 1
-    cuboid = np.pad(cuboid, 1)
+                ans1 += 1
+    xl, yl, zl, xh, yh, zh = xl - 1, yl - 1, zl - 1, xh + 1, yh + 1, zh + 1
 
-    # flood fill outside of cuboid
-    seed = (0, 0, 0)
-    s = {seed}
+    ans2 = 0
+    q = deque([(xl, yl, zl)])
     done = set()
-    n = 0
-    while n != len(s):
-        n = len(s)
-        s2 = set()
-        for a, b, c in s:
-            if (a, b, c) in done:
+    while q:
+        i, j, k = q.pop()
+        if (i, j, k) in done:
+            continue
+        for other in (
+            (i + 1, j, k),
+            (i - 1, j, k),
+            (i, j + 1, k),
+            (i, j - 1, k),
+            (i, j, k + 1),
+            (i, j, k - 1),
+        ):
+            if not (xl <= i <= xh and yl <= j <= yh and zl <= k <= zh):
                 continue
-            for other in [
-                (a + 1, b, c),
-                (a - 1, b, c),
-                (a, b + 1, c),
-                (a, b - 1, c),
-                (a, b, c + 1),
-                (a, b, c - 1),
-            ]:
-                if (
-                    other not in s
-                    and all(0 <= i < 22 for i in other)
-                    and cuboid[other] == 0
-                ):
-                    s2.add(other)
-        s |= s2
-        done.add((a, b, c))
+            elif other in cubes:
+                ans2 += 1
+            else:
+                q.append(other)
+        done.add((i, j, k))
 
-    # part 2
-    outer = np.zeros_like(cuboid)
-    for x in s:
-        outer[x] = 1
-    S2 = 0
-    for e in np.argwhere(outer):
-        a, b, c = e
-        for other in [
-            (a + 1, b, c),
-            (a - 1, b, c),
-            (a, b + 1, c),
-            (a, b - 1, c),
-            (a, b, c + 1),
-            (a, b, c - 1),
-        ]:
-            if all(0 <= i < 22 for i in other) and outer[other] == 0:
-                S2 += 1
-    return S, S2
+    return ans1, ans2
