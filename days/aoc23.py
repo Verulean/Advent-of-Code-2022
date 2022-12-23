40a5500ad90de0612f728f44da377490f92dcbdc
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-23 01:10:18 -0600

    Update aoc23.py

--- days/aoc23.py
+++ days/aoc23.py
@@ -1,59 +1,97 @@
-from collections import defaultdict
-from util import lmap
-import numpy as np
+from collections import defaultdict, deque
+
+
+class GameOfElf:
+    __neighbors = (
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
+    )
+
+    def __init__(self, elves):
+        self.__directions = deque(
+            [
+                ({0, 1, 2}, -1, 0),
+                ({4, 5, 6}, 1, 0),
+                ({6, 7, 0}, 0, -1),
+                ({2, 3, 4}, 0, 1),
+            ]
+        )
+        self.__step = 0
+        self.__min_i = None
+        self.__max_i = None
+        self.__min_j = None
+        self.__max_j = None
+        self.__elves = elves
+        for i, j in self.__elves:
+            self.__update_extrema(i, j)
+
+    def __update_extrema(self, i, j):
+        if self.__min_i is None or i < self.__min_i:
+            self.__min_i = i
+        if self.__max_i is None or i > self.__max_i:
+            self.__max_i = i
+        if self.__min_j is None or j < self.__min_j:
+            self.__min_j = j
+        if self.__max_j is None or j > self.__max_j:
+            self.__max_j = j
+
+    def __empty_area(self):
+        return (self.__max_i - self.__min_i + 1) * (
+            self.__max_j - self.__min_j + 1
+        ) - len(self.__elves)
+
+    def __elf_indices(self, i, j):
+        return {
+            k
+            for k, (di, dj) in enumerate(self.__neighbors)
+            if (i + di, j + dj) in self.__elves
+        }
+
+    def step(self):
+        self.__step += 1
+        next_elves = set()
+        proposals = defaultdict(set)
+        for i, j in self.__elves:
+            elf_indices = self.__elf_indices(i, j)
+            if not elf_indices:
+                next_elves.add((i, j))
+                continue
+            for indices, di, dj in self.__directions:
+                if not indices & elf_indices:
+                    proposals[(i + di, j + dj)].add((i, j))
+                    break
+            else:
+                next_elves.add((i, j))
+        moved = False
+        for next_pos, elves in proposals.items():
+            if len(elves) > 1:
+                next_elves.update(elves)
+            else:
+                next_elves.add(next_pos)
+                self.__update_extrema(*next_pos)
+                moved = True
+        self.__elves = next_elves
+        self.__directions.rotate(-1)
+        return moved
+
+    def solve(self, target_step=10):
+        ans1 = None
+        while self.step():
+            if self.__step == 10:
+                ans1 = self.__empty_area()
+        return ans1, self.__step
 
 
 def solve(data):
-    N = 1000
-    grid = np.where(np.array(lmap(list, data)) == "#", 1, 0)
-    grid = np.pad(grid, N + 1)
-
-    DIRS = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]])
-    ans2 = None
-    for step in range(N):
-        proposals = defaultdict(set)
-        stays = set()
-        for i, j in np.argwhere(grid):
-            if np.count_nonzero(grid[i - 1 : i + 2, j - 1 : j + 2]) <= 1:
-                stays.add((i, j))
-                continue
-            for di, dj in DIRS:
-                if di == 0:
-                    if np.count_nonzero(grid[i - 1 : i + 2, j + dj]) == 0:
-                        proposals[(i + di, j + dj)].add((i, j))
-                        break
-                if dj == 0:
-                    if np.count_nonzero(grid[i + di, j - 1 : j + 2]) == 0:
-                        proposals[(i + di, j + dj)].add((i, j))
-                        break
-            else:
-                stays.add((i, j))
-        # check proposals
-        next_grid = np.zeros_like(grid)
-        moved = False
-        for elf in stays:
-            next_grid[elf] = 1
-        for next_pos, elves in proposals.items():
-            if len(elves) > 1:
-                for elf in elves:
-                    next_grid[elf] = 1
-            elif len(elves) == 1:
-                next_grid[next_pos] = 1
-                moved = True
-        if not moved and ans2 is None:
-            ans2 = step + 1
-            break
-        grid = next_grid
-        DIRS = np.roll(DIRS, -1, axis=0)
-    imin = 999999999
-    imax = 0
-    jmin = 999999999
-    jmax = 0
-    for i, j in np.argwhere(grid):
-        imin = min(imin, i)
-        imax = max(imax, i)
-        jmin = min(jmin, j)
-        jmax = max(jmax, j)
-
-    grid = grid[imin : imax + 1, jmin : jmax + 1]
-    return np.count_nonzero(1 - grid), ans2
+    elves = set()
+    for i, line in enumerate(data):
+        for j, c in enumerate(line):
+            if c == "#":
+                elves.add((i, j))
+    return GameOfElf(elves).solve()
