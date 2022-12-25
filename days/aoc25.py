835695f83c535823ce8f49454ef0fe3d0b5f2927
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-24 23:44:47 -0600

    Update aoc25.py

--- days/aoc25.py
+++ days/aoc25.py
@@ -1,14 +1,7 @@
-from math import ceil, log
-from util import smap
-
-
 class SNAFU:
     __base = 5
     __digits = {"=": -2, "-": -1, "0": 0, "1": 1, "2": 2}
     __rev_digits = {v: k for k, v in __digits.items()}
-    __zero = __rev_digits[0]
-    __min_dec = min(__rev_digits)
-    __max_dec = max(__rev_digits)
 
     @staticmethod
     def snafu2dec(n: str) -> int:
@@ -16,25 +9,11 @@ class SNAFU:
     
     @staticmethod
     def dec2snafu(n: int) -> str:
-        max_pow = ceil(log(n, SNAFU.__base))
-        min_vals = [0]
-        max_vals = [0]
-        for p in range(max_pow):
-            min_vals.append(min_vals[-1] + SNAFU.__min_dec * SNAFU.__base ** p)
-            max_vals.append(max_vals[-1] + SNAFU.__max_dec * SNAFU.__base ** p)
-        candidates = {"": 0}
-        for p in range(max_pow, -1, -1):
-            mult = SNAFU.__base ** p
-            new_candidates = {}
-            for snafu, dec in candidates.items():
-                for digit, value in SNAFU.__digits.items():
-                    new_value = dec + value * mult
-                    delta = n - new_value
-                    if delta == 0:
-                        return (snafu + digit + SNAFU.__zero * p).lstrip(SNAFU.__zero)
-                    if min_vals[p] <= delta <= max_vals[p]:
-                        new_candidates[snafu + digit] = new_value
-            candidates = new_candidates
+        ret = ""
+        while n:
+            ret += SNAFU.__rev_digits[(n + 2) % 5 - 2]
+            n //= SNAFU.__base
+        return ret[::-1]
 
 def solve(data):
-    return SNAFU.dec2snafu(smap(SNAFU.snafu2dec, data))
+    return SNAFU.dec2snafu(sum(map(SNAFU.snafu2dec, data)))
