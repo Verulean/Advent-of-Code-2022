df7bfb81cdfa96a2975e149e69a6522ae415f4be
Verulean <65474222+Verulean@users.noreply.github.com>
2022-12-25 01:45:07 -0600

    Update aoc25.py

--- days/aoc25.py
+++ days/aoc25.py
@@ -1,18 +1,16 @@
-from collections import defaultdict
 from math import ceil, log
 
 
 def solve(data):
+    max_digits = max(map(len, data)) + ceil(log(len(data), 5))
+    digits = [0] * max_digits
     s2d = {"=": -2, "-": -1, "0": 0, "1": 1, "2": 2}
     d2s = {d: s for s, d in s2d.items()}
-    digits = defaultdict(int)
-    max_len = 0
     for n in data:
-        max_len = max(max_len, len(n))
         for i, c in enumerate(reversed(n)):
             digits[i] += s2d[c]
-    for i in range(max_len + ceil(log(len(data), 5)) + 1):
+    for i in range(max_digits - 1):
         q, r = divmod(digits[i] + 2, 5)
         digits[i] = r - 2
         digits[i + 1] += q
-    return "".join(d2s[d] for d in reversed(digits.values())).lstrip("0")
+    return "".join(map(d2s.get, reversed(digits))).lstrip("0")
